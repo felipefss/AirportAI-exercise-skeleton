b1866607875f8ba6f8cd9aed3b5c7460b25a921e
Felipe Santos <felipefss@gmail.com>
2024-09-09 16:52:29 +0100

    update auth middlewares

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,9 +1,18 @@
-function verifyToken(req, res, next) {
+function getUserRole(req, res, next) {
   const token = req.header('Authorization');
   if (token === 'some-hash') {
+    req.userRole = 'admin';
+  }
+
+  return next();
+}
+
+function denyUnauthorized(req, res, next) {
+  if (req.userRole && req.userRole === 'admin') {
     return next();
   }
+
   return res.sendStatus(401);
 }
 
-module.exports = { verifyToken };
+module.exports = { getUserRole, denyUnauthorized };
--- server/routes/index.js
+++ server/routes/index.js
@@ -7,12 +7,7 @@ let express = require('express');
 let router = express.Router();
 const authRoutes = require('./auth');
 
-const { verifyToken } = require('../middlewares/auth');
-
 router.use('/auth', authRoutes);
-
-// The following routes are protected by the verifyToken middleware
-router.use(verifyToken);
 
 router.get('*', function (req, res) {
   return res.sendStatus(404);
