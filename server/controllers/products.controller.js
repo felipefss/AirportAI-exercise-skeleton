0b5cc65083b72419e546c0c4ca185ce2af9be79a
Felipe Santos <felipefss@gmail.com>
2024-09-10 10:18:02 +0100

    refactor product controller

--- server/controllers/products.controller.js
+++ server/controllers/products.controller.js
@@ -1,17 +1,54 @@
+const { z } = require('zod');
+
 const Product = require('../models/Product');
 
 // TODO: consider when the product is created via a passenger reporting a lost item.
-async function createProduct(payload) {
-  const product = new Product(payload);
-  return await product.save();
+async function createProduct(req, res) {
+  const productSchema = z.object({
+    item: z.string(),
+    brand: z.string().optional(),
+    model: z.string().optional(),
+    color: z.string().optional(),
+    description: z.string(),
+  });
+
+  const parsedPayload = productSchema.safeParse(req.body);
+
+  if (!parsedPayload.success) {
+    return res.status(400).send(parsedPayload.error.flatten().fieldErrors);
+  }
+
+  // Create the product in the database.
+  const product = new Product(parsedPayload.data);
+  const createdProduct = await product.save();
+  console.info('Product created:', createdProduct);
+  return res.sendStatus(201);
 }
 
-async function getProducts() {
-  return await Product.find();
+async function getProducts(_, res) {
+  const products = await Product.find();
+
+  return res.send(products);
 }
 
-async function deleteProduct(id) {
-  return await Product.findByIdAndDelete(id);
+async function deleteProduct(req, res) {
+  const deleteSchema = z.object({
+    id: z.string(),
+  });
+
+  const parsedParams = deleteSchema.safeParse(req.params);
+
+  if (!parsedParams.success) {
+    return res.status(400).send(parsedParams.error.flatten().fieldErrors);
+  }
+
+  const deletedProduct = await Product.findByIdAndDelete(parsedParams.data.id);
+
+  if (!deletedProduct) {
+    return res.sendStatus(404);
+  }
+
+  return res.sendStatus(204);
 }
 
 module.exports = {
--- server/routes/products.route.js
+++ server/routes/products.route.js
@@ -1,6 +1,5 @@
 let express = require('express');
 let router = express.Router();
-const { z } = require('zod');
 
 const { denyUnauthorized } = require('../middlewares/auth');
 const { createProduct, getProducts, deleteProduct } = require('../controllers/products.controller');
@@ -8,36 +7,17 @@ const { createProduct, getProducts, deleteProduct } = require('../controllers/pr
 router.use(denyUnauthorized);
 
 router.post('/', async (req, res) => {
-  const productSchema = z.object({
-    item: z.string(),
-    brand: z.string().optional(),
-    model: z.string().optional(),
-    color: z.string().optional(),
-    description: z.string(),
-  });
-
-  const product = productSchema.safeParse(req.body);
-
-  if (!product.success) {
-    return res.status(400).send(product.error.flatten().fieldErrors);
-  }
-
-  // Create the product in the database.
   try {
-    const createdProduct = await createProduct(product.data);
-    console.info('Product created:', createdProduct);
+    return await createProduct(req, res);
   } catch (error) {
     console.error(error);
     return res.sendStatus(500);
   }
-
-  return res.sendStatus(201);
 });
 
 router.get('/', async (req, res) => {
   try {
-    const products = await getProducts();
-    return res.send(products);
+    return await getProducts(req, res);
   } catch (error) {
     console.error(error);
     return res.sendStatus(500);
@@ -45,28 +25,12 @@ router.get('/', async (req, res) => {
 });
 
 router.delete('/:id', async (req, res) => {
-  const deleteSchema = z.object({
-    id: z.string(),
-  });
-
-  const data = deleteSchema.safeParse(req.params);
-
-  if (!data.success) {
-    return res.status(400).send(data.error.flatten().fieldErrors);
-  }
-
   try {
-    const deletedProduct = await deleteProduct(data.data.id);
-
-    if (!deletedProduct) {
-      return res.sendStatus(404);
-    }
+    return await deleteProduct(req, res);
   } catch (error) {
     console.error(error);
     return res.sendStatus(500);
   }
-
-  return res.sendStatus(204);
 });
 
 module.exports = router;
